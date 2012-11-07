9d571d21f4d2e757b812cde5d3bae0c7d3f9f85e
Pawankumar Dhamane <pawankumardhamane@gmail.com>
2012-11-07 19:46:11 +0530

    Added bluetooth connection code

--- GUI/Mobile/Android/StartLED/gen/com/example/theone/R.java
+++ GUI/Mobile/Android/StartLED/gen/com/example/theone/R.java
@@ -16,20 +16,24 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f070000;
-        public static final int menu_settings=0x7f07000e;
+        public static final int menu_settings=0x7f070012;
         public static final int radioMode0=0x7f070002;
         public static final int radioMode1=0x7f070003;
         public static final int radioMode2=0x7f070004;
         public static final int radioMode3=0x7f070005;
         public static final int radioSex=0x7f070001;
-        public static final int seekBarBlue=0x7f07000b;
-        public static final int seekBarDelay=0x7f07000d;
-        public static final int seekBarGreen=0x7f070009;
-        public static final int seekBarRed=0x7f070007;
-        public static final int textViewBlue=0x7f07000a;
-        public static final int textViewDelay=0x7f07000c;
-        public static final int textViewGreen=0x7f070008;
+        public static final int seekBarBlue=0x7f07000e;
+        public static final int seekBarDelay=0x7f070011;
+        public static final int seekBarGreen=0x7f07000b;
+        public static final int seekBarRed=0x7f070008;
+        public static final int textViewBlue=0x7f07000c;
+        public static final int textViewBluevalue=0x7f07000d;
+        public static final int textViewDelay=0x7f07000f;
+        public static final int textViewDelayvalue=0x7f070010;
+        public static final int textViewGreen=0x7f070009;
+        public static final int textViewGreenvalue=0x7f07000a;
         public static final int textViewRed=0x7f070006;
+        public static final int textViewRedvalue=0x7f070007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,12 +43,20 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int blue=0x7f04000b;
+        public static final int bluevalue=0x7f04000d;
+        public static final int connected=0x7f040008;
+        public static final int delay=0x7f04000c;
+        public static final int delayvalue=0x7f04000f;
+        public static final int green=0x7f04000a;
+        public static final int greenvalue=0x7f04000e;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int mode0=0x7f040003;
         public static final int mode1=0x7f040004;
         public static final int mode2=0x7f040005;
         public static final int mode3=0x7f040006;
+        public static final int red=0x7f040009;
         public static final int title_activity_main=0x7f040007;
     }
     public static final class style {
--- GUI/Mobile/Android/StartLED/src/com/example/theone/MainActivity.java
+++ GUI/Mobile/Android/StartLED/src/com/example/theone/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.theone;
 
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,8 +28,25 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 public class MainActivity extends Activity {
 
 	Button button;
+	
+	private static final String TAG = MainActivity.class.getCanonicalName();
+	
+	private static final int BARREL_LEFT  = 1;
+	private static final int BARREL_RIGHT = 2;
+	
+	private static final int MENU_CONNECT = 1;
+	
 	private RadioGroup radioModeGroup;
 	private RadioButton radioSexButton;
+	private BluetoothDevice m_Cannon;
+	private ConnectTask		m_ConTask;
+	private boolean 		m_isConnected = false;
+	
+    private BluetoothSocket m_BtSocket;
+    private OutputStream 	m_Output;
+   
+
+    private FireTask		m_FireTask;
 	SeekBar seekbarred;
 	SeekBar seekbargreen;
 	SeekBar seekbarblue;
@@ -49,9 +75,16 @@ public class MainActivity extends Activity {
 //				Intent browserIntent = 
 //                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://about.me/the1pawan"));
 //				startActivity(browserIntent);
-				for(int i = 0; i < radioModeGroup.getChildCount(); i++){
-				    ((RadioButton)radioModeGroup.getChildAt(i)).setEnabled(true);
-				}
+				
+				//hardware device
+				//m_Cannon = BluetoothAdapter.getDefaultAdapter().getRemoteDevice("00:19:A4:02:44:2A");
+				//Android tab
+				//m_Cannon = BluetoothAdapter.getDefaultAdapter().getRemoteDevice("A0:75:91:53:6F:2F");
+				//Murali PC
+				m_Cannon = BluetoothAdapter.getDefaultAdapter().getRemoteDevice("00:15:83:15:A3:10");
+		        
+		        connect();
+				
 			}
 		});
     	
@@ -63,18 +96,28 @@ public class MainActivity extends Activity {
 					seekbarred.setVisibility(View.GONE);
 			        seekbarblue.setVisibility(View.GONE);
 			        seekbargreen.setVisibility(View.GONE);
+			        valueRed.setText(" value is 0");
+			        valueBlue.setText(" value is 0");
+			        valueGreen.setText(" value is 0");
 					Toast.makeText(MainActivity.this,"Mode0", Toast.LENGTH_SHORT).show();
+					fire(1);
 				}
 				else if (checkedId==R.id.radioMode1) {
 					seekbarred.setVisibility(View.GONE);
 			        seekbarblue.setVisibility(View.GONE);
 			        seekbargreen.setVisibility(View.GONE);
+			        valueRed.setText(" value is 0");
+			        valueBlue.setText(" value is 0");
+			        valueGreen.setText(" value is 0");
 					Toast.makeText(MainActivity.this,"Mode1", Toast.LENGTH_SHORT).show();
 				} 
 				else if (checkedId==R.id.radioMode2) {
 					seekbarred.setVisibility(View.GONE);
 			        seekbarblue.setVisibility(View.GONE);
 			        seekbargreen.setVisibility(View.GONE);
+			        valueRed.setText(" value is 0");
+			        valueBlue.setText(" value is 0");
+			        valueGreen.setText(" value is 0");
 					Toast.makeText(MainActivity.this,"Mode2", Toast.LENGTH_SHORT).show();
 				}
 				else if (checkedId==R.id.radioMode3) {
@@ -87,7 +130,7 @@ public class MainActivity extends Activity {
 		});
     	
     	 seekbarred = (SeekBar) findViewById(R.id.seekBarRed);
-    	 valueRed=(TextView) findViewById(R.id.textViewRed);
+    	 valueRed=(TextView) findViewById(R.id.textViewRedvalue);
     	 seekbarred.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
     	 
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -103,12 +146,12 @@ public class MainActivity extends Activity {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				// TODO Auto-generated method stub
-				valueRed.setText("SeekBar value is "+progress);
+				valueRed.setText(" value is "+progress);
 			}
 		});
     	 
     	 seekbargreen=(SeekBar) findViewById(R.id.seekBarGreen);
-    	 valueGreen=(TextView) findViewById(R.id.textViewGreen);
+    	 valueGreen=(TextView) findViewById(R.id.textViewGreenvalue);
     	 seekbargreen.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -124,12 +167,12 @@ public class MainActivity extends Activity {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				// TODO Auto-generated method stub
-				valueGreen.setText("SeekBar value is "+progress);
+				valueGreen.setText(" value is "+progress);
 			}
 		});
     	 
     	 seekbarblue=(SeekBar) findViewById(R.id.seekBarBlue);
-    	 valueBlue=(TextView) findViewById(R.id.textViewBlue);
+    	 valueBlue=(TextView) findViewById(R.id.textViewBluevalue);
     	 seekbarblue.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -145,12 +188,12 @@ public class MainActivity extends Activity {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				// TODO Auto-generated method stub
-				valueBlue.setText("SeekBar value is "+progress);
+				valueBlue.setText(" value is "+progress);
 			}
 		});
     	 
     	 seekbardelay=(SeekBar) findViewById(R.id.seekBarDelay);
-    	 valueDelay=(TextView) findViewById(R.id.textViewDelay);
+    	 valueDelay=(TextView) findViewById(R.id.textViewDelayvalue);
     	 seekbardelay.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -166,11 +209,136 @@ public class MainActivity extends Activity {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				// TODO Auto-generated method stub
-				valueDelay.setText("SeekBar value is "+progress);
+				valueDelay.setText(" value is "+progress);
 			}
 		});
     }
     
+    public void connect()
+	{
+		if (m_isConnected)
+		{
+			Log.d(TAG,"Bluetooth is already connected");
+			return;
+		}		
+		
+		if(m_ConTask == null || m_ConTask.getStatus() != AsyncTask.Status.RUNNING) {
+			if(m_ConTask != null) {
+				m_ConTask.cancel(true);
+			}
+			m_ConTask = new ConnectTask();
+			m_ConTask.execute((Void)null);
+		}
+	}
+    
+    public void checkButtons() {
+		if(m_isConnected) {
+			for(int i = 0; i < radioModeGroup.getChildCount(); i++){
+			    ((RadioButton)radioModeGroup.getChildAt(i)).setEnabled(true);
+			}
+		}
+		else {
+			for(int i = 0; i < radioModeGroup.getChildCount(); i++){
+			    ((RadioButton)radioModeGroup.getChildAt(i)).setEnabled(false);
+			}
+		}
+	}
+    
+    private class ConnectTask extends AsyncTask<Void,String,Void> {
+		ProgressDialog mDialog;
+		
+		protected void onPreExecute() {
+			mDialog = ProgressDialog.show(MainActivity.this,null,"Connecting to Cannon",true,true);
+		}
+		protected Void doInBackground(Void... arg0) {
+			Log.d(TAG,"ConnectTask started");
+			if (m_isConnected)
+			{
+				return null;
+			}
+			
+			try
+			{		    	
+		        Method m = m_Cannon.getClass().getMethod("createRfcommSocket", new Class[] { int.class });
+		        m_BtSocket = (BluetoothSocket) m.invoke(m_Cannon, Integer.valueOf(1));
+			    Log.d(TAG, "Bluetooth is connecting...");
+			        
+			    m_BtSocket.connect();
+	            publishProgress("The Bluetooth connection is good!");
+			    m_isConnected = true;
+			    return null;
+			}
+			catch (Exception e)
+			{
+	            publishProgress("The Bluetooth connection failed.");
+				m_isConnected = false;
+				Log.e(TAG, e.toString());
+				return null;
+			}
+		}
+		
+		protected void onProgressUpdate(String...values)
+		{
+			Toast.makeText(MainActivity.this, values[0], Toast.LENGTH_LONG).show();
+		}
+		
+		protected void onPostExecute(Void Result)
+		{
+			mDialog.cancel();
+			checkButtons();
+			Log.d(TAG,"ConnectTask finished");
+		}
+	}
+    
+    public void fire(int barrel)
+	{
+        if(m_FireTask == null || m_FireTask.getStatus() != AsyncTask.Status.RUNNING)
+        	if(m_FireTask != null) {
+        		m_FireTask.cancel(true);
+        	}
+        	m_FireTask = new FireTask();
+        	m_FireTask.execute(barrel);
+    }
+    
+    private class FireTask extends AsyncTask<Integer,String,Void> {
+		protected Void doInBackground(Integer...args) {
+			Log.d(TAG,"FireTask started for barrel " + args[0]);
+			
+	        try
+	        {	        	
+	        	if(!m_isConnected) {
+	        		publishProgress("Not connected. Cannot fire.");
+					Log.e(TAG, "Tried to fire without being connected");
+					return null;
+	        	}
+	        	
+			    Log.d(TAG, "Bluetooth is connected!");
+			        
+			    m_Output = m_BtSocket.getOutputStream();
+			    char cmd = 'm';
+			    m_Output.write(cmd);
+			    Log.d(TAG,"BT write successful. CMD=" + cmd);
+			    return null;
+	        }
+	        catch (Exception e)
+	        {
+	        	publishProgress("The fire command could not be sent. Try reconnecting.");
+	        	Log.e(TAG, e.toString());
+	        	Log.d(TAG,"Returning to unconnected state");
+	        	m_isConnected = false;
+	        	checkButtons();
+	        	return null;
+	        }			
+		}
+		
+		protected void onProgressUpdate(String...strings) {
+			Toast.makeText(MainActivity.this,strings[0],Toast.LENGTH_LONG).show();
+		}
+		
+		protected void onPostExecute(Void result) {
+			Log.d(TAG,"FireTask finished");
+		}
+	}
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
